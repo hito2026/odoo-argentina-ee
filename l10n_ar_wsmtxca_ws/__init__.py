a960cad6c452a74ffce1cc63fc266c22f0c0d1a2
Jinzo Implementador <jinzo@hitofusion.com>
2026-09-10 20:19:58 +0000

    [FIX] l10n_ar_wsmtxca_ws: monkey patch ARTransport.post con huge_tree=True
    Al validar facturas electronicas wsmtxca, AFIP devuelve XML con
    profundidad mayor a 256 niveles, lo que provoca error de lxml:
    "Excessive depth in document: 256 use XML_PARSE_HUGE option".
    
    Se aplica monkey patch sobre ARTransport.post (l10n_ar_edi) para
    parsear la respuesta con etree.XMLParser(huge_tree=True).

--- l10n_ar_wsmtxca_ws/__init__.py
+++ l10n_ar_wsmtxca_ws/__init__.py
@@ -4,3 +4,24 @@
 ##############################################################################
 from . import models
 from . import wizards
+
+# Monkey patch: permitir XML de respuesta de AFIP con profundidad > 256 niveles
+# Soluciona: "Excessive depth in document: 256 use XML_PARSE_HUGE option"
+from lxml import etree
+from odoo.addons.l10n_ar_edi.models.l10n_ar_afipws_connection import ARTransport
+
+_original_ar_transport_post = ARTransport.post
+
+
+def _patched_ar_transport_post(self, address, message, headers):
+    response = _original_ar_transport_post(self, address, message, headers)
+    # Re-parsear la respuesta con huge_tree=True para soportar XML profundo
+    parser = etree.XMLParser(huge_tree=True)
+    self.xml_response = etree.tostring(
+        etree.fromstring(response.content, parser=parser),
+        pretty_print=True,
+    ).decode("utf-8")
+    return response
+
+
+ARTransport.post = _patched_ar_transport_post
